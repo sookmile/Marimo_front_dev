94fa4c6b5d8aeb6d89fe0c7f053a0fc35b805ea9
bsorinnn <bxxsxrxx@gmail.com>
2021-09-14 23:22:52 +0900

    feat: Hangul 라이브러리 테스트

--- src/components/Game/GameMain.js
+++ src/components/Game/GameMain.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React from "react";
+import { TouchableOpacity } from "react-native";
 import {
   StyleSheet,
   Text,
@@ -8,23 +8,23 @@ import {
   SafeAreaView,
   Image,
   FlatList,
-} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const ListItem1 = ({ item }) => {
   const navigation = useNavigation();
   return (
-    <TouchableOpacity onPress={() => navigation.navigate(item.route)}>
-    <View style={styles.item}>
-      <Image
-        source={{
-          uri: item.uri,
-        }}
-        style={styles.itemPhoto}
-        resizeMode="cover"
-      />
-      <Text style={styles.itemText}>{item.text}</Text>
-    </View>
+    <TouchableOpacity onPress={() => navigation.navigate("SpellingGame")}>
+      <View style={styles.item}>
+        <Image
+          source={{
+            uri: item.uri,
+          }}
+          style={styles.itemPhoto}
+          resizeMode="cover"
+        />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
@@ -33,16 +33,16 @@ const ListItem2 = ({ item }) => {
   const navigation = useNavigation();
   return (
     <TouchableOpacity onPress={() => navigation.navigate(item.route)}>
-    <View style={styles.friend}>
-      <Image
-        source={{
-          uri: item.uri,
-        }}
-        style={styles.friendPic}
-        resizeMode="cover"
-      />
-      <Text style={styles.itemText}>{item.text}</Text>
-    </View>
+      <View style={styles.friend}>
+        <Image
+          source={{
+            uri: item.uri,
+          }}
+          style={styles.friendPic}
+          resizeMode="cover"
+        />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
@@ -53,7 +53,7 @@ const StoryMain = () => {
       <View style={styles.header}>
         <Image
           style={styles.mainLogo}
-          source={require('../../assets/icons/MainLogo.png')}
+          source={require("../../assets/icons/MainLogo.png")}
         />
         <Text>마리모와 말의 세계</Text>
       </View>
@@ -61,7 +61,7 @@ const StoryMain = () => {
         <View style={styles.name}>
           <Image
             style={styles.logo}
-            source={require('../../assets/icons/Logo.png')}
+            source={require("../../assets/icons/Logo.png")}
           />
           <Text style={styles.userName}>송이</Text>
         </View>
@@ -83,30 +83,30 @@ const StoryMain = () => {
         </View>
         <View style={styles.storyBlock}>
           <SafeAreaView style={{ flex: 1 }}>
-        <SectionList
-          contentContainerStyle={{ paddingHorizontal: 10 }}
-          stickySectionHeadersEnabled={false}
-          sections={SECTIONS1}
-          renderSectionHeader={({ section }) => (
-            <>
-              {section.horizontal ? (
-                <FlatList
-                  horizontal
-                  data={section.data}
-                  renderItem={({ item }) => <ListItem1 item={item} />}
-                  showsHorizontalScrollIndicator={false}
-                />
-              ) : null}
-            </>
-          )}
-          renderItem={({ item, section }) => {
-            if (section.horizontal) {
-              return null;
-            }
-            return <ListItem1 item={item} />;
-          }}
-        />
-      </SafeAreaView>
+            <SectionList
+              contentContainerStyle={{ paddingHorizontal: 10 }}
+              stickySectionHeadersEnabled={false}
+              sections={SECTIONS1}
+              renderSectionHeader={({ section }) => (
+                <>
+                  {section.horizontal ? (
+                    <FlatList
+                      horizontal
+                      data={section.data}
+                      renderItem={({ item }) => <ListItem1 item={item} />}
+                      showsHorizontalScrollIndicator={false}
+                    />
+                  ) : null}
+                </>
+              )}
+              renderItem={({ item, section }) => {
+                if (section.horizontal) {
+                  return null;
+                }
+                return <ListItem1 item={item} />;
+              }}
+            />
+          </SafeAreaView>
         </View>
       </View>
       <View>
@@ -118,30 +118,30 @@ const StoryMain = () => {
         </View>
         <View style={styles.storyBlock}>
           <SafeAreaView style={{ flex: 1 }}>
-        <SectionList
-          contentContainerStyle={{ paddingHorizontal: 10 }}
-          stickySectionHeadersEnabled={false}
-          sections={SECTIONS2}
-          renderSectionHeader={({ section }) => (
-            <>
-              {section.horizontal ? (
-                <FlatList
-                  horizontal
-                  data={section.data}
-                  renderItem={({ item }) => <ListItem2 item={item} />}
-                  showsHorizontalScrollIndicator={false}
-                />
-              ) : null}
-            </>
-          )}
-          renderItem={({ item, section }) => {
-            if (section.horizontal) {
-              return null;
-            }
-            return <ListItem2 item={item} />;
-          }}
-        />
-      </SafeAreaView>
+            <SectionList
+              contentContainerStyle={{ paddingHorizontal: 10 }}
+              stickySectionHeadersEnabled={false}
+              sections={SECTIONS2}
+              renderSectionHeader={({ section }) => (
+                <>
+                  {section.horizontal ? (
+                    <FlatList
+                      horizontal
+                      data={section.data}
+                      renderItem={({ item }) => <ListItem2 item={item} />}
+                      showsHorizontalScrollIndicator={false}
+                    />
+                  ) : null}
+                </>
+              )}
+              renderItem={({ item, section }) => {
+                if (section.horizontal) {
+                  return null;
+                }
+                return <ListItem2 item={item} />;
+              }}
+            />
+          </SafeAreaView>
         </View>
       </View>
     </View>
@@ -152,79 +152,77 @@ export default StoryMain;
 
 const SECTIONS1 = [
   {
-    title: 'Made for you',
+    title: "Made for you",
     horizontal: true,
     data: [
       {
-        key: '1',
-        text: '누가누가 잘하나, 재미있는 끝말잇기',
-        uri: 'https://picsum.photos/id/1/200',
-        route: ''
+        key: "1",
+        text: "누가누가 잘하나, 재미있는 끝말잇기",
+        uri: "https://picsum.photos/id/1/200",
+        route: "",
       },
       {
-        key: '2',
-        text: '동물 친구들의 초성 게임',
-        uri: 'https://picsum.photos/id/10/200',
-        route: ''
+        key: "2",
+        text: "동물 친구들의 초성 게임",
+        uri: "https://picsum.photos/id/10/200",
+        route: "",
       },
 
       {
-        key: '3',
-        text: '충치를 막아라, 치카치카 방어대',
-        uri: 'https://picsum.photos/id/1002/200',
-        route: ''
+        key: "3",
+        text: "충치를 막아라, 치카치카 방어대",
+        uri: "https://picsum.photos/id/1002/200",
+        route: "",
       },
     ],
   },
-  
 ];
 
 const SECTIONS2 = [
   {
-    title: 'Made for you',
+    title: "Made for you",
     horizontal: true,
     data: [
       {
-        key: '1',
-        text: '연우',
-        uri: 'https://picsum.photos/id/1/200',
-        route: ''
+        key: "1",
+        text: "연우",
+        uri: "https://picsum.photos/id/1/200",
+        route: "",
       },
       {
-        key: '2',
-        text: '하준',
-        uri: 'https://picsum.photos/id/10/200',
-        route: ''
+        key: "2",
+        text: "하준",
+        uri: "https://picsum.photos/id/10/200",
+        route: "",
       },
 
       {
-        key: '3',
-        text: '지아',
-        uri: 'https://picsum.photos/id/1002/200',
-        route: ''
+        key: "3",
+        text: "지아",
+        uri: "https://picsum.photos/id/1002/200",
+        route: "",
       },
       {
-        key: '4',
-        text: '세빈',
-        uri: 'https://picsum.photos/id/1002/200',
-        route: ''
+        key: "4",
+        text: "세빈",
+        uri: "https://picsum.photos/id/1002/200",
+        route: "",
       },
     ],
   },
-  
 ];
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     zIndex: -1,
-    display: 'flex',
-    padding: 10
+    display: "flex",
+    padding: 10,
   },
   header: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
     marginLeft: 5,
     height: 50,
   },
@@ -237,91 +235,91 @@ const styles = StyleSheet.create({
   logo: {
     width: 50,
     height: 50,
-    backgroundColor: '#A098FD',
+    backgroundColor: "#A098FD",
     borderRadius: 45,
     marginLeft: 5,
   },
   name: {
-    display: 'flex',
-    flexDirection: 'row',
-    backgroundColor: '#F1DFFF',
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#F1DFFF",
     borderRadius: 45,
     borderWidth: 3,
-    borderColor: '#C5A1F3',
+    borderColor: "#C5A1F3",
     marginLeft: 5,
     marginRight: 5,
   },
   userName: {
-    textAlign: 'center',
-    textAlignVertical: 'center',
+    textAlign: "center",
+    textAlignVertical: "center",
     width: 300,
   },
   records: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginTop: 5,
   },
   rButton1: {
     width: 114,
     height: 36,
     borderRadius: 40,
-    backgroundColor: '#FF8C73',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#FF8C73",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   rButton2: {
     width: 114,
     height: 36,
     borderRadius: 40,
-    backgroundColor: '#FEBB61',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#FEBB61",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     justifyContent: "space-between",
-    margin: 10
+    margin: 10,
   },
   storyBlock: {
     width: 370,
     height: 200,
-    display: 'flex',
-    justifyContent: 'space-around',
+    display: "flex",
+    justifyContent: "space-around",
     marginLeft: 10,
   },
   item: {
     marginRight: 10,
     width: 140,
     height: 195,
-    backgroundColor: '#FAEBFF',
-    display: 'flex',
-    alignItems: 'center',
+    backgroundColor: "#FAEBFF",
+    display: "flex",
+    alignItems: "center",
     borderRadius: 30,
   },
   itemPhoto: {
     width: 140,
     height: 135,
-    borderRadius: 20
+    borderRadius: 20,
   },
   itemText: {
-    color: 'gray',
+    color: "gray",
     marginTop: 5,
   },
   friend: {
     width: 85,
     height: 105,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 10
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
   },
   friendPic: {
     width: 80,
     height: 80,
     borderRadius: 50,
-  }
+  },
 });
--- src/components/Game/SpellingGame.js
+++ src/components/Game/SpellingGame.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -6,22 +6,91 @@ import {
   Button,
   Image,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
+import hangul from "hangul-js";
+import Voice from "react-native-voice";
+import { SIZES } from "../../constants";
+import CustomButton from "../CustomButton/CustomButton";
 
-function SpellingGame({navigation}) {
-  return (
-    <View style={{flex: 1, zIndex: -1, display: 'flex'}}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('SpellingGameResult')}>
-        <Image
+function SpellingGame({ navigation }) {
+  const [isRecord, setisRecord] = useState(false);
+  const [text, setText] = useState("");
+  const buttonLabel = isRecord ? "Stop" : "Start";
+  const voiceLabel = text
+    ? text
+    : isRecord
+    ? "Say something"
+    : "press Start Button";
+  const _onSpeechStart = () => {
+    console.log("onSpeechStart");
+    setText("");
+  };
+  const _onSpeechEnd = () => {
+    console.log("onSpeechEnd");
+  };
+  const _onSpeechResults = (event) => {
+    console.log("onSpeechResults");
+    setText(event.value[0]);
+  };
+  const _onSpeechError = (event) => {
+    console.log("_onSpeechError");
+    console.log(event.error);
+  };
+
+  const _onRecordVoice = () => {
+    if (isRecord) {
+      Voice.stop();
+    } else {
+      Voice.start("ko-KR");
+    }
+    setisRecord(!isRecord);
+  };
+
+  useEffect(() => {
+    Voice.onSpeechStart = _onSpeechStart;
+    Voice.onSpeechEnd = _onSpeechEnd;
+    Voice.onSpeechResults = _onSpeechResults;
+    Voice.onSpeechError = _onSpeechError;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const renderHeader = () => {
+    return (
+      <View style={{ height: SIZES.height > 700 ? "65%" : "60%" }}>
+        <Text
           style={{
-            width: '100%',
-            height: '100%',
+            marginTop: SIZES.radius,
+            paddingVertical: SIZES.padding,
+            paddingHorizontal: SIZES.padding,
           }}
-          source={require('../../assets/gameWFriend.png')}
-          resizeMode="cover"
-        />
-      </TouchableOpacity>
+        >
+          {voiceLabel}
+        </Text>
+        <Text>{text && _onSpeechEnd && diassembleWords(text)}</Text>
+        <CustomButton buttonText={buttonLabel} onPress={_onRecordVoice} />
+      </View>
+    );
+  };
+
+  const diassembleWords = (text) => {
+    let diassemble = hangul.disassemble(text, true);
+
+    let cho = "";
+    if (diassemble) {
+      console.log(diassemble);
+      for (let i = 0, l = diassemble.length; i < l; i++) {
+        cho += diassemble[i][0];
+      }
+      console.log(cho);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, zIndex: -1, display: "flex" }}>
+      {renderHeader()}
     </View>
   );
 }
