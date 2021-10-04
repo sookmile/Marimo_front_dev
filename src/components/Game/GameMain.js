2575721fe724d3a6ab25493583f3cef4cdee321f
변우진 <sforot9@gmail.com>
2021-10-04 10:35:15 +0900

    style : 전체 UI 변경

--- src/components/Game/GameMain.js
+++ src/components/Game/GameMain.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TouchableOpacity } from "react-native";
 import {
   StyleSheet,
@@ -7,189 +7,229 @@ import {
   SectionList,
   SafeAreaView,
   Image,
+  StatusBar,
+  ScrollView,
   FlatList,
 } from "react-native";
+import { SIZES, COLORS, navTabIcons } from "../../constants";
 import { fontPercentage } from "../../constants/responsive";
-import { SIZES, COLORS } from "../../constants";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 import { useNavigation } from "@react-navigation/native";
+import Orientation from "react-native-orientation";
 import { UserHeader } from "../UserHeader";
-import {
-  heightPercentageToDP,
-  widthPercentageToDP,
-} from "react-native-responsive-screen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import styled, { css } from "styled-components";
 
-const ListItem1 = ({ item }) => {
+const ListItem = ({ item }) => {
+  const navigation = useNavigation();
+
+  return (
+    <ItemButton label={item.label}>
+      <ItemBox
+        background={item.background}
+        onPress={() => navigation.navigate(`${item.router}`)}
+      >
+        <Image source={item.src} style={styles.itemPhoto} resizeMode="cover" />
+        <ItemText color={item.color}>{item.label}</ItemText>
+      </ItemBox>
+    </ItemButton>
+  );
+};
+
+const renderItem = ({ item }) => {
   const navigation = useNavigation();
   return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate("SpellingGameContainer")}
+    <View
+      style={{
+        alignContent: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 16,
+      }}
     >
-      <View style={styles.item}>
-        <Image
-          source={{
-            uri: item.uri,
+      <ContnetSubCntr onPress={() => navigation.navigate(`${item.router}`)}>
+        <ChImage
+          style={{
+            borderRadius: 20,
+            width: 90,
+            height: 90,
           }}
-          style={styles.itemPhoto}
-          resizeMode="cover"
+          source={item.src}
         />
-        <Text style={styles.itemText}>{item.text}</Text>
-      </View>
-    </TouchableOpacity>
+        <ContentTexts>
+          <ContentTitle numberOfLines={1} ellipsizeMode="tail">
+            {item.text}
+          </ContentTitle>
+          <ContentText>추천 연령 : {item.age}세</ContentText>
+        </ContentTexts>
+      </ContnetSubCntr>
+    </View>
   );
 };
+const ContnetSubCntr = styled.TouchableOpacity`
+  width: 100%;
+  height: 108px;
+  background: #fbf8ff;
+  border-radius: 23;
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+  elevation: 10;
+  flex-direction: row;
+  padding-horizontal: 10;
+  margin-horizontal: 10;
+`;
+const ChImage = styled(Image)`
+  width: 20%;
+`;
+const ContentTexts = styled.View`
+  width: 65%;
+`;
+const ContentTitle = styled.Text`
+  font-family: Noto Sans CJK KR;
+  margin-bottom: 15;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 24px;
+  color: #000000;
+  overflow: hidden;
+`;
 
-const ListItem2 = ({ item }) => {
-  const navigation = useNavigation();
+const ContentText = styled.Text`
+  font-family: Noto Sans CJK KR;
+  font-weight: ${(props) => (props.isTitle ? "700" : "400")};
+  margin-bottom: ${(props) => (props.isTitle ? 15 : 0)};
+  font-size: 14px;
+  line-height: 23px;
+  color: #434141;
+`;
+
+const GameMain = () => {
+  const [userNickname, setUserNickName] = useState("");
+  useEffect(() => {
+    Orientation.lockToPortrait();
+    Orientation.addOrientationListener(onOrientaionChange);
+    return () => {
+      Orientation.unlockAllOrientations(),
+        Orientation.removeOrientationListener(onOrientaionChange);
+    };
+  });
+  const onOrientaionChange = (orientation) => {
+    if (orientation === "PORTRAIT") {
+      Orientation.lockToPortrait();
+    }
+  };
+  useEffect(async () => {
+    const Nickname = await AsyncStorage.getItem("userNickname");
+    console.log(Nickname);
+    setUserNickName(Nickname);
+  }, []);
   return (
-    <TouchableOpacity onPress={() => navigation.navigate(item.route)}>
-      <View style={styles.friend}>
-        <Image
-          source={{
-            uri: item.uri,
+    <ScrollView>
+      <View style={styles.container}>
+        <View
+          style={{
+            display: "flex",
+            flex: 1,
+            width: "92%",
+            alignContent: "center",
+            alignItems: "center",
           }}
-          style={styles.friendPic}
-          resizeMode="cover"
-        />
-        <Text style={styles.itemText}>{item.text}</Text>
-      </View>
-    </TouchableOpacity>
-  );
-};
-
-const StoryMain = () => {
-  const navigation = useNavigation();
-  const userNickname = "송이";
-  return (
-    <SafeAreaView style={styles.container}>
-      {/* <View style={styles.header}>
-        <Image
-          style={styles.mainLogo}
-          source={require("../../assets/icons/MainLogo.png")}
-        />
-        <Text>마리모와 말의 세계</Text>
-      </View>
-      <View>
-        <View style={styles.name}>
-          <Image
-            style={styles.logo}
-            source={require("../../assets/icons/Logo.png")}
+        >
+          <UserHeader
+            style={{ height: "40%" }}
+            type={"game"}
+            userNickname={userNickname}
           />
-          <Text style={styles.userName}>송이</Text>
-        </View>
-        <View style={styles.records}>
-          <TouchableOpacity
-            onPress={() => navigation.navigate("LearnRecord")}
-            style={styles.rButton1}
+          <View
+            style={{
+              width: "94%",
+              height: "57%",
+              alignContent: "center",
+              alignItems: "center",
+            }}
           >
-            <Text>나의 기록들</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.rButton2}>
-            <Text>친구 등록하기</Text>
-          </TouchableOpacity>
-        </View>
-      </View> */}
-      <UserHeader userNickname={userNickname} />
-      <View>
-        <View style={styles.title}>
-          <Text style={styles.titleText}>슝슝슝, 게임하면서 놀아요!</Text>
-          <TouchableOpacity>
-            <Text>+</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.storyBlock}>
-          <SafeAreaView style={{ flex: 1 }}>
-            <SectionList
-              contentContainerStyle={{ paddingHorizontal: 10 }}
-              stickySectionHeadersEnabled={false}
-              sections={SECTIONS1}
-              renderSectionHeader={({ section }) => (
-                <>
-                  {section.horizontal ? (
-                    <FlatList
-                      horizontal
-                      data={section.data}
-                      renderItem={({ item }) => <ListItem1 item={item} />}
-                      showsHorizontalScrollIndicator={false}
-                    />
-                  ) : null}
-                </>
-              )}
-              renderItem={({ item, section }) => {
-                if (section.horizontal) {
-                  return null;
-                }
-                return <ListItem1 item={item} />;
+            <View
+              style={{
+                display: "flex",
+                flex: 1,
+                marginBottom: 5,
               }}
-            />
-          </SafeAreaView>
+            >
+              <View
+                style={{
+                  display: "flex",
+                }}
+              >
+                <StudyTxt>추천 학습</StudyTxt>
+              </View>
+              <View
+                stlye={{
+                  flex: 1,
+                  marginTop: StatusBar.currentHeight || 0,
+                  marginBottom: 5,
+                }}
+              >
+                {SECTIONS3.map((item) => renderItem({ item }))}
+              </View>
+            </View>
+          </View>
         </View>
       </View>
-      <View>
-        <View style={styles.title}>
-          <Text style={styles.titleText}>나의 친구들</Text>
-          <TouchableOpacity>
-            <Text>+</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.storyBlock}>
-          <SafeAreaView style={{ flex: 1 }}>
-            <SectionList
-              contentContainerStyle={{ paddingHorizontal: 10 }}
-              stickySectionHeadersEnabled={false}
-              sections={SECTIONS2}
-              renderSectionHeader={({ section }) => (
-                <>
-                  {section.horizontal ? (
-                    <FlatList
-                      horizontal
-                      data={section.data}
-                      renderItem={({ item }) => <ListItem2 item={item} />}
-                      showsHorizontalScrollIndicator={false}
-                    />
-                  ) : null}
-                </>
-              )}
-              renderItem={({ item, section }) => {
-                if (section.horizontal) {
-                  return null;
-                }
-                return <ListItem2 item={item} />;
-              }}
-            />
-          </SafeAreaView>
-        </View>
-      </View>
-    </SafeAreaView>
+      <Wrapper />
+    </ScrollView>
   );
 };
 
-export default StoryMain;
+export default GameMain;
 
 const SECTIONS1 = [
   {
-    title: "Made for you",
-    horizontal: true,
-    data: [
-      {
-        key: "1",
-        text: "누가누가 잘하나, 재미있는 끝말잇기",
-        uri: "https://picsum.photos/id/1/200",
-        route: "",
-      },
-      {
-        key: "2",
-        text: "동물 친구들의 초성 게임",
-        uri: "https://picsum.photos/id/10/200",
-        route: "",
-      },
-
-      {
-        key: "3",
-        text: "충치를 막아라, 치카치카 방어대",
-        uri: "https://picsum.photos/id/1002/200",
-        route: "",
-      },
-    ],
+    key: "1",
+    label: "동화",
+    src: navTabIcons.ic_story,
+    color: "#CCAB37",
+    background: "rgba(251, 222, 120, 0.08)",
+    router: "Story",
+  },
+  {
+    key: "2",
+    label: "게임",
+    src: navTabIcons.ic_game,
+    color: "#D5A0FE",
+    background: "rgba(213, 160, 254, 0.08)",
+    router: "Game",
+  },
+  {
+    key: "3",
+    label: "탐험",
+    src: navTabIcons.ic_camera,
+    color: "#F66C6C",
+    background: "rgba(246, 108, 108, 0.08)",
+    router: "Explore",
+  },
+];
+const SECTIONS3 = [
+  {
+    key: "1",
+    text: "누가누가 잘하나 재미있는 끝말잇기",
+    age: "6~7",
+    src: navTabIcons.ic_game1,
+    router: "SpellingGameContainer",
+  },
+  {
+    key: "2",
+    text: "동물 친구들의 초성게임",
+    age: "7~8",
+    src: navTabIcons.ic_game2,
+    router: "SpellingGameContainer",
+  },
+  {
+    key: "3",
+    text: "충치를 막아라, 치카치카 방어대",
+    src: navTabIcons.ic_game3,
+    age: "7~8",
+    router: "SpellingGameContainer",
   },
 ];
 
@@ -200,28 +240,22 @@ const SECTIONS2 = [
     data: [
       {
         key: "1",
-        text: "연우",
-        uri: "https://picsum.photos/id/1/200",
-        route: "",
+        text: "이상한 나라의 앨리스",
+        src: "https://picsum.photos/id/1/200",
+        route: "StoryLoading",
       },
       {
         key: "2",
-        text: "하준",
+        text: "호두까기 인형",
         uri: "https://picsum.photos/id/10/200",
-        route: "",
+        route: "StoryLoading",
       },
 
       {
         key: "3",
-        text: "지아",
+        text: "Item text 3",
         uri: "https://picsum.photos/id/1002/200",
-        route: "",
-      },
-      {
-        key: "4",
-        text: "세빈",
-        uri: "https://picsum.photos/id/1002/200",
-        route: "",
+        route: "StoryLoading",
       },
     ],
   },
@@ -230,9 +264,9 @@ const SECTIONS2 = [
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    zIndex: -1,
     display: "flex",
     padding: 10,
+    alignItems: "center",
   },
   header: {
     display: "flex",
@@ -307,33 +341,29 @@ const styles = StyleSheet.create({
   },
   storyBlock: {
     width: 370,
-    height: 200,
+    height: "40%",
+    display: "flex",
+    justifyContent: "space-around",
+    marginLeft: 10,
+  },
+  studyBlock: {
+    width: "92%",
+    height: "40%",
     display: "flex",
     justifyContent: "space-around",
     marginLeft: 10,
   },
   item: {
-    marginRight: 10,
-    width: 140,
-    height: heightPercentageToDP(35),
+    width: "100%",
+    height: 145,
+    backgroundColor: "#FAEBFF",
     display: "flex",
     alignItems: "center",
-    borderRadius: 30,
-    backgroundColor: "#FAEBFF",
   },
-  // item: {
-  //   marginRight: 10,
-  //   width: 140,
-  //   height: 195,
-  //   backgroundColor: "#FAEBFF",
-  //   display: "flex",
-  //   alignItems: "center",
-  //   borderRadius: 30,
-  // },
   itemPhoto: {
-    width: 140,
-    height: 135,
-    borderRadius: 20,
+    width: 64,
+    height: 63,
+    borderRadius: 23.5,
   },
   itemText: {
     textAlign: "center",
@@ -341,19 +371,43 @@ const styles = StyleSheet.create({
     color: "gray",
     marginTop: 10,
     fontFamily: "NanumSquareRoundB",
-    fontSize: widthPercentageToDP(3.5),
-  },
-  friend: {
-    width: 85,
-    height: 105,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginRight: 10,
-  },
-  friendPic: {
-    width: 80,
-    height: 80,
-    borderRadius: 50,
+    fontSize: wp(3.5),
   },
 });
+const ItemBox = styled.TouchableOpacity`
+  width: 97px;
+  height: 105px;
+
+  margin-right: 10;
+  background: ${(props) => props.background};
+  border-radius: 20px;
+  border-color: ${(props) => props.background};
+  align-items: center;
+  align-content: center;
+`;
+const ItemText = styled.Text`
+  color: ${(props) => props.color};
+  text-align: center;
+  padding-horizontal: 5;
+  margin-top: 10;
+  font-family: NanumSquareRoundB;
+  font-size: 18;
+  font-weight: bold;
+`;
+
+const ItemButton = styled.View`
+  margin-right: ${(props) => (props.label !== "탐험" ? 15 : 0)};
+  overflow: visible;
+`;
+const StudyTxt = styled.Text`
+  font-family: NanumSquareRoundB;
+  font-size: 22px;
+  line-height: 28px;
+  font-weight: bold;
+  color: #191919;
+`;
+
+const Wrapper = styled.View`
+  width: 100%;
+  height: 100;
+`;
--- src/components/Story/StoryMain.js
+++ src/components/Story/StoryMain.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { TouchableOpacity } from "react-native";
 import {
   StyleSheet,
@@ -7,153 +7,176 @@ import {
   SectionList,
   SafeAreaView,
   Image,
+  StatusBar,
+  ScrollView,
   FlatList,
 } from "react-native";
-import { SIZES, COLORS } from "../../constants";
+import { SIZES, COLORS, navTabIcons } from "../../constants";
 import { fontPercentage } from "../../constants/responsive";
 import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 import { useNavigation } from "@react-navigation/native";
 import Orientation from "react-native-orientation";
 import { UserHeader } from "../UserHeader";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import styled, { css } from "styled-components";
 
 const ListItem = ({ item }) => {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity onPress={() => navigation.navigate("StoryLoading")}>
-      <View style={styles.item}>
-        <Image
-          source={{
-            uri: item.uri,
-          }}
-          style={styles.itemPhoto}
-          resizeMode="cover"
-        />
-        <Text style={styles.itemText}>{item.text}</Text>
-      </View>
-    </TouchableOpacity>
+    <ItemButton label={item.label}>
+      <ItemBox
+        background={item.background}
+        onPress={() => navigation.navigate(`${item.router}`)}
+      >
+        <Image source={item.src} style={styles.itemPhoto} resizeMode="cover" />
+        <ItemText color={item.color}>{item.label}</ItemText>
+      </ItemBox>
+    </ItemButton>
   );
 };
 
+const renderItem = ({ item }) => {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        alignContent: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 16,
+      }}
+    >
+      <ContnetSubCntr onPress={() => navigation.navigate(`${item.router}`)}>
+        <ChImage
+          style={{
+            borderRadius: 20,
+            width: 90,
+            height: 90,
+          }}
+          source={item.src}
+        />
+        <ContentTexts>
+          <ContentTitle numberOfLines={1} ellipsizeMode="tail">
+            {item.text}
+          </ContentTitle>
+          <ContentText>추천 연령 : {item.age}세</ContentText>
+        </ContentTexts>
+      </ContnetSubCntr>
+    </View>
+  );
+};
+const ContnetSubCntr = styled.TouchableOpacity`
+  width: 100%;
+  height: 108px;
+  background: #fbf8ff;
+  border-radius: 23;
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+  elevation: 10;
+  flex-direction: row;
+  padding-horizontal: 10;
+  margin-horizontal: 10;
+`;
+const ChImage = styled(Image)`
+  width: 20%;
+`;
+const ContentTexts = styled.View`
+  width: 65%;
+`;
+const ContentTitle = styled.Text`
+  font-family: Noto Sans CJK KR;
+  margin-bottom: 15;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 24px;
+  color: #000000;
+`;
+
+const ContentText = styled.Text`
+  font-family: Noto Sans CJK KR;
+  font-weight: ${(props) => (props.isTitle ? "700" : "400")};
+  margin-bottom: ${(props) => (props.isTitle ? 15 : 0)};
+  font-size: 14px;
+  line-height: 23px;
+  color: #434141;
+`;
+
 const StoryMain = () => {
+  const [userNickname, setUserNickName] = useState("");
   useEffect(() => {
     Orientation.lockToPortrait();
     Orientation.addOrientationListener(onOrientaionChange);
-    return (
-      () => {
-        Orientation.unlockAllOrientations(),
-          Orientation.removeOrientationListener(onOrientaionChange);
-      },
-      []
-    );
+    return () => {
+      Orientation.unlockAllOrientations(),
+        Orientation.removeOrientationListener(onOrientaionChange);
+    };
   });
   const onOrientaionChange = (orientation) => {
-    if (orientation === "LANDSCAPE") {
+    if (orientation === "PORTRAIT") {
       Orientation.lockToPortrait();
     }
   };
-  const userNickname = "송이";
+  useEffect(async () => {
+    const Nickname = await AsyncStorage.getItem("userNickname");
+    console.log(Nickname);
+    setUserNickName(Nickname);
+  }, []);
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <View style={styles.header}>
-        <Image
-          style={styles.mainLogo}
-          source={require("../../assets/icons/MainLogo.png")}
-        />
-        <Text>마리모와 말의 세계</Text>
-      </View>
-      <View>
-        <View style={styles.name}>
-          <Image
-            style={styles.logo}
-            source={require("../../assets/icons/Logo.png")}
+    <ScrollView>
+      <View style={styles.container}>
+        <View
+          style={{
+            display: "flex",
+            flex: 1,
+            width: "92%",
+            alignContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <UserHeader
+            style={{ height: "40%" }}
+            type={"story"}
+            userNickname={userNickname}
           />
-          <Text style={styles.userName}>송이</Text>
-        </View>
-        <View style={styles.records}>
-          <TouchableOpacity
-            style={styles.rButton1}
-            onPress={() => navigation.navigate("LearnRecord")}
+          <View
+            style={{
+              width: "94%",
+              height: "57%",
+              alignContent: "center",
+              alignItems: "center",
+            }}
           >
-            <Text>나의 기록들</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.rButton2}>
-            <Text>친구 등록하기</Text>
-          </TouchableOpacity>
-        </View>
-      </View> */}
-      <UserHeader userNickname={userNickname} />
-      <View>
-        <View style={styles.title}>
-          <Text style={styles.titleText}>신나게 움직여요. 마리모 탐험대!</Text>
-          <TouchableOpacity>
-            <Text>+</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.storyBlock}>
-          <SafeAreaView style={{ flex: 1 }}>
-            <SectionList
-              contentContainerStyle={{ paddingHorizontal: 10 }}
-              stickySectionHeadersEnabled={false}
-              sections={SECTIONS1}
-              renderSectionHeader={({ section }) => (
-                <>
-                  {section.horizontal ? (
-                    <FlatList
-                      horizontal
-                      data={section.data}
-                      renderItem={({ item }) => <ListItem item={item} />}
-                      showsHorizontalScrollIndicator={false}
-                    />
-                  ) : null}
-                </>
-              )}
-              renderItem={({ item, section }) => {
-                if (section.horizontal) {
-                  return null;
-                }
-                return <ListItem item={item} />;
+            <View
+              style={{
+                display: "flex",
+                flex: 1,
+                marginBottom: 5,
               }}
-            />
-          </SafeAreaView>
+            >
+              <View
+                style={{
+                  display: "flex",
+                }}
+              >
+                <StudyTxt>추천 학습</StudyTxt>
+              </View>
+              <View
+                stlye={{
+                  flex: 1,
+                  marginTop: StatusBar.currentHeight || 0,
+                  marginBottom: 5,
+                }}
+              >
+                {SECTIONS3.map((item) => renderItem({ item }))}
+              </View>
+            </View>
+          </View>
         </View>
       </View>
-      <View>
-        <View style={styles.title}>
-          <Text style={styles.titleText}>이야기 속으로, 신비한 동화나라!</Text>
-          <TouchableOpacity>
-            <Text>+</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.storyBlock}>
-          <SafeAreaView style={{ flex: 1 }}>
-            <SectionList
-              contentContainerStyle={{ paddingHorizontal: 10 }}
-              stickySectionHeadersEnabled={false}
-              sections={SECTIONS2}
-              renderSectionHeader={({ section }) => (
-                <>
-                  {section.horizontal ? (
-                    <FlatList
-                      horizontal
-                      data={section.data}
-                      renderItem={({ item }) => <ListItem item={item} />}
-                      showsHorizontalScrollIndicator={false}
-                    />
-                  ) : null}
-                </>
-              )}
-              renderItem={({ item, section }) => {
-                if (section.horizontal) {
-                  return null;
-                }
-                return <ListItem item={item} />;
-              }}
-            />
-          </SafeAreaView>
-        </View>
-      </View>
-    </SafeAreaView>
+      <Wrapper />
+    </ScrollView>
   );
 };
 
@@ -161,29 +184,54 @@ export default StoryMain;
 
 const SECTIONS1 = [
   {
-    title: "Made for you",
-    horizontal: true,
-    data: [
-      {
-        key: "1",
-        text: "앗, 도와줘! 우당탕탕 왕국 모험",
-        uri: "https://picsum.photos/id/1/200",
-        route: "Story1",
-      },
-      {
-        key: "2",
-        text: "마리모 친구들의 얼음성 대탐험!",
-        uri: "https://picsum.photos/id/10/200",
-        route: "StoryLoading",
-      },
-
-      {
-        key: "3",
-        text: "Item text 3",
-        uri: "https://picsum.photos/id/1002/200",
-        route: "StoryLoading",
-      },
-    ],
+    key: "1",
+    label: "동화",
+    src: navTabIcons.ic_story,
+    color: "#CCAB37",
+    background: "rgba(251, 222, 120, 0.08)",
+    router: "Story",
+  },
+  {
+    key: "2",
+    label: "게임",
+    src: navTabIcons.ic_game,
+    color: "#D5A0FE",
+    background: "rgba(213, 160, 254, 0.08)",
+    router: "Game",
+  },
+  {
+    key: "3",
+    label: "탐험",
+    src: navTabIcons.ic_camera,
+    color: "#F66C6C",
+    background: "rgba(246, 108, 108, 0.08)",
+    router: "Explore",
+  },
+];
+const SECTIONS3 = [
+  {
+    key: "1",
+    text: "앗, 도와줘! 우당탕탕 왕국 모험",
+    age: "6~7",
+    src: navTabIcons.ic_story1,
+    number: 50,
+    router: "StoryLoading",
+  },
+  {
+    key: "2",
+    text: "이상한 나라의 앨리스",
+    age: "7~8",
+    src: navTabIcons.ic_story2,
+    number: 20,
+    router: "StoryLoading",
+  },
+  {
+    key: "3",
+    text: "호두까기 인형",
+    src: navTabIcons.ic_story3,
+    age: "7~8",
+    number: 20,
+    router: "StoryLoading",
   },
 ];
 
@@ -195,7 +243,7 @@ const SECTIONS2 = [
       {
         key: "1",
         text: "이상한 나라의 앨리스",
-        uri: "https://picsum.photos/id/1/200",
+        src: "https://picsum.photos/id/1/200",
         route: "StoryLoading",
       },
       {
@@ -218,9 +266,9 @@ const SECTIONS2 = [
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    zIndex: -1,
     display: "flex",
     padding: 10,
+    alignItems: "center",
   },
   header: {
     display: "flex",
@@ -295,24 +343,29 @@ const styles = StyleSheet.create({
   },
   storyBlock: {
     width: 370,
-    height: 145,
+    height: "40%",
+    display: "flex",
+    justifyContent: "space-around",
+    marginLeft: 10,
+  },
+  studyBlock: {
+    width: "92%",
+    height: "40%",
     display: "flex",
     justifyContent: "space-around",
     marginLeft: 10,
   },
   item: {
-    marginRight: 10,
-    width: 204,
+    width: "100%",
     height: 145,
     backgroundColor: "#FAEBFF",
     display: "flex",
     alignItems: "center",
-    borderRadius: 30,
   },
   itemPhoto: {
-    width: 195,
-    height: 104,
-    borderRadius: 20,
+    width: 64,
+    height: 63,
+    borderRadius: 23.5,
   },
   itemText: {
     textAlign: "center",
@@ -323,3 +376,40 @@ const styles = StyleSheet.create({
     fontSize: wp(3.5),
   },
 });
+const ItemBox = styled.TouchableOpacity`
+  width: 97px;
+  height: 105px;
+
+  margin-right: 10;
+  background: ${(props) => props.background};
+  border-radius: 20px;
+  border-color: ${(props) => props.background};
+  align-items: center;
+  align-content: center;
+`;
+const ItemText = styled.Text`
+  color: ${(props) => props.color};
+  text-align: center;
+  padding-horizontal: 5;
+  margin-top: 10;
+  font-family: NanumSquareRoundB;
+  font-size: 18;
+  font-weight: bold;
+`;
+
+const ItemButton = styled.View`
+  margin-right: ${(props) => (props.label !== "탐험" ? 15 : 0)};
+  overflow: visible;
+`;
+const StudyTxt = styled.Text`
+  font-family: NanumSquareRoundB;
+  font-size: 22px;
+  line-height: 28px;
+  font-weight: bold;
+  color: #191919;
+`;
+
+const Wrapper = styled.View`
+  width: 100%;
+  height: 100;
+`;
