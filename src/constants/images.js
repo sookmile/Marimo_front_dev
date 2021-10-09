0d7312ff42fd7d7b29f3b36e8e5e96072e04e87f
bsorinnn <bxxsxrxx@gmail.com>
2021-10-10 04:27:18 +0900

    style: 이미지 추가

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,3 +1,7 @@
+//splash screen
+const splashScreen = require("../assets/marimo_splash.png");
+const readyScreenBackground = require("../assets/readyBackground.png");
+
 const sunflowers = require("../assets/images/memories/sunflowers.png");
 const deer = require("../assets/images/memories/deer.png");
 const blocks = require("../assets/images/memories/blocks.png");
@@ -5,8 +9,6 @@ const blocks = require("../assets/images/memories/blocks.png");
 //  Game Objects
 const gameBackground = require("../assets/images/GameObject/BG.png");
 const gameOb_wordContainer = require("../assets/images/GameObject/Window.png");
-const gameOb_Crate = require("../assets/images/GameObject/Crate.png");
-const gameOb_tile2 = require("../assets/images/GameObject/2.png");
 
 // SpellingGame Objects
 const spellingGameBackground = require("../assets/images/GameObject/SpellingGame/background.png");
@@ -20,12 +22,12 @@ const scoreFailed = require("../assets/images/GameObject/SpellingGame/scoreEggFa
 const scoreSuccess = require("../assets/images/GameObject/SpellingGame/scoreEgg.png");
 
 export default {
+  splashScreen,
+  readyScreenBackground,
   sunflowers,
   deer,
   blocks,
   gameOb_wordContainer,
-  gameOb_Crate,
-  gameOb_tile2,
   gameBackground,
   spellingGameBackground,
   gameWordBoard,
