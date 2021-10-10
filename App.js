a0a116d40c4b796035e41f9fc37456fd459f85b9
변우진 <sforot9@gmail.com>
2021-10-10 17:11:29 +0900

    feat : Home 화면에서 바로가기 링크 추가
    - 동화, 게임 모두 Home에서 접근가능하도록 설정

--- App.js
+++ App.js
@@ -3,13 +3,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 // import SplashScreen from 'react-native-splash-screen';
 
-//Screens
+// Contents
 import Start from "./src/screens/StartScreen";
 import Story from "./src/screens/StoryScreen";
 import Game from "./src/screens/GameScreen";
+// Screens
 import Explore from "./src/screens/ExploreScreen";
 import SettingScreen from "./src/screens/SettingScreen";
-
+// Story
+import StoryLoading from "./src/components/Story/StoryLoading";
+import StoryMain from "./src/components/Story/StoryMain";
+import StoryOne from "./src/components/Story/StoryOne";
+import Practice from "./src/components/Story/Practice";
+// Game
+import SpellingGame from "./src/components/Game/SpellingGame";
+import SpellingGameResult from "./src/components/Game/SpellingGameResult";
+import SpellingGameRanking from "./src/components/Game/SpellingGameRanking";
+import GameMain from "./src/components/Game/GameMain";
+import SpellingGameContainer from "./src/components/Game/SpellingGameContainer";
 //Tabs
 import NavTab from "./src/screens/NavTabs";
 
@@ -59,6 +70,52 @@ const App = () => {
         <MainStack.Screen
           name="SettingScreen"
           component={SettingScreen}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen
+          name="StoryMain"
+          component={StoryMain}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen
+          name="StoryLoading"
+          component={StoryLoading}
+          options={{ headerShown: false, tabBarVisible: false }}
+        />
+        <MainStack.Screen
+          name="Story1"
+          component={StoryOne}
+          options={{ headerShown: false, tabBarVisible: false }}
+        />
+        <MainStack.Screen
+          name="Practice"
+          component={Practice}
+          options={{ headerShown: false, tabBarVisible: false }}
+        />
+
+        <MainStack.Screen
+          name="GameMain"
+          component={GameMain}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen
+          name="SpellingGameContainer"
+          component={SpellingGameContainer}
+          options={{ headerShown: true, title: "냠냠 맛있는 모음게임임" }}
+        />
+        <MainStack.Screen
+          name="SpellingGame"
+          component={SpellingGame}
+          options={{ headerShown: true, title: "냠냠 맛있는 모음게임" }}
+        />
+        <MainStack.Screen
+          name="GameResult"
+          component={SpellingGameResult}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen
+          name="GameRank"
+          component={SpellingGameRanking}
           options={{ headerShown: false }}
         />
       </MainStack.Navigator>
