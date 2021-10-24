2aaf7ef4eec13cfcd77d1ca579b62def22785d96
bsorinnn <bxxsxrxx@gmail.com>
2021-10-25 02:11:51 +0900

    feat: 오픈 소스 라이브러리 디자인 변경

--- src/screens/Settings/Details.js
+++ src/screens/Settings/Details.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Card } from "react-native-elements";
 
 function Details({ route }) {
-  const { licenseUrl, licenses, parents, repository } = route.params;
+  const { licenseUrl, licenses, parents, repository, name } = route.params;
   return (
-    <View style={{ flex: 1 }}>
+    // <View style={{ flex: 1, bacg }}>
+    //   <Text>{licenseUrl}</Text>
+    //   <Text>{licenses}</Text>
+    //   <Text>{parents}</Text>
+    //   <Text>{repository}</Text>
+    // </View>
+    <Card>
+      <Card.Title>LICENSE</Card.Title>
+      <Card.Divider />
       <Text>{licenseUrl}</Text>
       <Text>{licenses}</Text>
       <Text>{parents}</Text>
       <Text>{repository}</Text>
-    </View>
+    </Card>
   );
 }
 
